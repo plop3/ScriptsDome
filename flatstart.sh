324833b654bf3c2aa88359b7847268a4767508fa
Astro <astro@astro>
2022-07-02 23:15:56 +0200

    Ajout scripts flat panel

---
+++ flatstart.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/usr/bin/indi_setprop -h astro.local "SnapCap.FLAT_LIGHT_INTENSITY.FLAT_LIGHT_INTENSITY_VALUE=250"
+/usr/bin/indi_setprop -h astro.local "SnapCap.FLAT_LIGHT_CONTROL.FLAT_LIGHT_ON=On"
+
---
+++ flatstop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/bin/indi_setprop -h astro.local "SnapCap.FLAT_LIGHT_CONTROL.FLAT_LIGHT_OFF=On"
+
